f4ed9ebd1ab0e1fd3639f59e0b69ef6049da18f5
nomnivore <6979410+nomnivore@users.noreply.github.com>
2024-12-03 01:28:58 -0500

    2-2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -20,63 +20,86 @@ fn report_lines(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
     separated_list1(alt((line_ending, eof)), report)(input)
 }
 
+fn is_safe(report: &Vec<u32>) -> bool {
+    // check if report is safe
+    // must be all decreasing or increasing
+    // diffs must be 1,2, or 3
+    let mut increasing: Option<bool> = None;
+    let mut prev: Option<&u32> = None;
+
+    for y in report {
+        // establish prev value
+        if prev.is_none() {
+            prev = Some(y);
+            continue;
+        }
+
+        let x = prev.unwrap();
+
+        // check diff between elements
+        let diff = x.abs_diff(*y);
+        if !(1..=3).contains(&diff) {
+            return false;
+        }
+
+        // handle inc/dec trend
+        // early return if fail condition
+        match increasing {
+            None => increasing = Some(y > x),
+            Some(true) => {
+                if x > y {
+                    return false;
+                };
+            }
+            Some(false) => {
+                if y > x {
+                    return false;
+                }
+            }
+        };
+
+        // assign next
+        prev = Some(y);
+    }
+
+    true
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let (_, lines) = report_lines(input).unwrap();
 
-    let res = lines
-        .into_iter()
-        .filter(|line| {
-            // check if report is safe
-            // must be all decreasing or increasing
-            // diffs must be 1,2, or 3
-            let mut increasing: Option<bool> = None;
-            let mut prev: Option<&u32> = None;
-
-            for y in line {
-                // establish prev value
-                if prev.is_none() {
-                    prev = Some(y);
-                    continue;
-                }
-
-                let x = prev.unwrap();
-
-                // check diff between elements
-                let diff = x.abs_diff(*y);
-                if !(1..=3).contains(&diff) {
-                    return false;
-                }
-
-                // handle inc/dec trend
-                // early return if fail condition
-                match increasing {
-                    None => increasing = Some(y > x),
-                    Some(true) => {
-                        if x > y {
-                            return false;
-                        };
-                    }
-                    Some(false) => {
-                        if y > x {
-                            return false;
-                        }
-                    }
-                };
-
-                // assign next
-                prev = Some(y);
-            }
-
-            true
-        })
-        .count();
+    let res = lines.into_iter().filter(is_safe).count();
 
     // bad error handling
     Some(res.try_into().unwrap())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (_, lines) = report_lines(input).unwrap();
+
+    let res = lines
+        .into_iter()
+        .filter(|report| {
+            // brute-force approach
+            if !is_safe(report) {
+                for idx in 0..report.len() {
+                    let mut report_copy = report.clone();
+                    report_copy.remove(idx);
+                    if is_safe(&report_copy) {
+                        return true;
+                    } else {
+                        continue;
+                    }
+                }
+                false
+            } else {
+                true
+            }
+        })
+        .count();
+
+    // bad error handling
+    Some(res.try_into().unwrap())
 }
 
 #[cfg(test)]
@@ -92,6 +115,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4u32));
     }
 }
